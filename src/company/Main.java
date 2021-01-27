66d054a8571323116eeb8a9c28172fe3f0239db0
vladis18 <vladisbabayan@gmail.com>
2021-01-26 20:45:47 -0500

    Solar system second commit

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,4 +1,10 @@
 package company;
 
 public class Main {
+    public static void main(String[] args) {
+        SolarSystem FactorX =new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
+    }
 }
